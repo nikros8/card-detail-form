be5b634c2151f05c37434a289ddd11c261493e01
nikolas <niko09185@gmail.com>
2024-01-08 23:50:44 +0100

    new components cardDetailForm, input

--- components/cardBack.vue
+++ components/cardBack.vue
@@ -1,28 +1,9 @@
-<script setup lang="ts">
-import cardBackImage from "~/assets/images/bg-card-back.png"
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div class="container">
-    <div class="cards">
-      <div class="card-back"></div>
-      <div class="card-front"></div>
-    </div>
-  </div>
+  <div class="card-back"></div>
 </template>
 
 <style scoped>
-.container {
-  max-width: 345px;
-  width: 100%;
-  margin-left: 16px;
-  margin-right: 16px;
-}
-.cards {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 30px;
-}
 .card-back {
   background-image: url(~/assets/images/bg-card-back.png);
   background-size: contain; /* or use other values like 'contain' */
@@ -30,15 +11,5 @@ import cardBackImage from "~/assets/images/bg-card-back.png"
   align-self: flex-end;
   width: 286px;
   height: 160px;
-}
-.card-front {
-  background-image: url(~/assets/images/bg-card-front.png);
-  background-size: contain; /* or use other values like 'contain' */
-  background-repeat: no-repeat;
-  align-self: flex-start;
-  position: relative;
-  bottom: 66px;
-  width: 286px;
-  height: 157px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts"></script>
 <template>
-  <main><cardBack /></main>
+  <main>
+    <div class="container">
+      <div class="cards">
+        <cardBack />
+        <cardFront />
+      </div>
+      <cardDetailForm />
+    </div>
+  </main>
 </template>
 <style scoped>
 main {
   display: flex;
   justify-content: center;
+}
+.container {
+  max-width: 345px;
+  width: 100%;
+  margin-left: 16px;
+  margin-right: 16px;
+}
+.cards {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 250px;
+  margin-top: 30px;
 }
 </style>
